ecdbee514c356b24c56c9f18dde35e97100b871a
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2021-08-13 10:08:10 +0200

    Dockerize

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM python:3.9
+
+WORKDIR /srv
+COPY index.py requirements.txt ./
+COPY apps apps/
+RUN pip install -r requirements.txt
+
+CMD ["gunicorn", "index:server", "--bind=0.0.0.0:8050", "--workers=4"]
--- index.py
+++ index.py
@@ -4,6 +4,7 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 
 app = dash.Dash(__name__, suppress_callback_exceptions=True)
+server = app.server
 
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
