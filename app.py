573c29b44b15cedf983261a3ada3eed1683e3b8a
Hayden Wilson <Hayden@SAEON.ac.za>
2021-08-18 08:30:26 +0200

    updated dependencies

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import dash
-
-app = dash.Dash(__name__, suppress_callback_exceptions=True)
+stylesheets = 'style.css'
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets, suppress_callback_exceptions=True)
 server = app.server
