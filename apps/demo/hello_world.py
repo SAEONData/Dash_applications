f75133eac7bdff85a73bd8ecbe80025e7810c4c4
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2021-09-13 11:54:44 +0200

    Create hello world demo app

---
+++ apps/demo/hello_world.py
@@ -0,0 +1,26 @@
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+from app import app
+
+data = {
+    1: 'One',
+    2: 'Two',
+    3: 'Three',
+    4: 'Four',
+}
+
+layout = html.Div([
+    html.H1('Hello World!'),
+    dcc.Slider(id='demo-slider', min=1, max=4, value=1),
+    html.H3(data[1], id='demo-text')
+])
+
+
+@app.callback(
+    Output('demo-text', 'children'),
+    Input('demo-slider', 'value')
+)
+def update_layout(input_value):
+    return data[input_value]
